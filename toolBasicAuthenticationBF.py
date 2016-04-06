7c4271173319751e790602cd750a563cba9e98c9
Stranar <agustinc5m@gmail.com>
2016-04-06 12:24:18 -0500

    Update toolBasicAuthenticationBF.py

--- toolBasicAuthenticationBF.py
+++ toolBasicAuthenticationBF.py
@@ -6,7 +6,6 @@ if len(argv) != 3:
 	URL = input('Insert URL: ')
 	fileUsers = input('Insert UserName File: ')
 	filePaswd = input('Insert password File: ')
-#print(URL +" "+ fileUsers + " "+filePaswd)
 userList = open(fileUsers, "r")
 paswdList = open(filePaswd, "r")
 users = userList.readlines()
@@ -15,7 +14,6 @@ passwords = paswdList.readlines()
 for user in users :
 	for paswd in passwords :
 		user_pass = "%s:%s"%(user.strip('\n'),paswd.strip('\n'))
-		#print(user_pass)
 		res = user_pass.encode(encoding='UTF-8')
 		base64_valueB = base64.b64encode(res)
 		base64_value = base64_valueB.decode("utf-8")

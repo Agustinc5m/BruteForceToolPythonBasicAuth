da445ac75626d07d2a33d1f0734338ed706a5218
Stranar <agustinc5m@gmail.com>
2016-04-07 09:18:57 -0500

    Borrado de comentarios

--- toolBasicAuthenticationBF.py
+++ toolBasicAuthenticationBF.py
@@ -7,12 +7,10 @@ if len(argv) != 3:
 	URL = input('Insert URL: ')
 	fileUsers = input('Insert UserName File: ')
 	filePaswd = input('Insert password File: ')
-#print(URL +" "+ fileUsers + " "+filePaswd)
 userList = open(fileUsers, "r")
 paswdList = open(filePaswd, "r")
 users = userList.readlines()
 passwords = paswdList.readlines()
-
 for user in users :
 	for paswd in passwords :
 		req = requests.get(URL, auth=HTTPBasicAuth(user.strip('\n'), paswd.strip('\n')))
